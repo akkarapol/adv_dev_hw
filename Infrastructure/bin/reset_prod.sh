ceceb2a43179f6b421944b6a65cf756202bfa5bd
LekWut <ukkarapol@gmail.com>
2019-01-04 22:25:53 +0700

    Update reset_prod.sh

--- Infrastructure/bin/reset_prod.sh
+++ Infrastructure/bin/reset_prod.sh
@@ -18,25 +18,6 @@ echo "Resetting Parks Production Environment in project ${GUID}-parks-prod to Gr
 # rollout followed by a Green rollout.
 
 # To be Implemented by Student
-oc get route/mlbparks -n ${GUID}-parks-prod |grep 'mlbparks-blue'
-if [ $? = true ]; then
-    echo "mlbparks-blue to mlbparks-green"
-    oc patch route/mlbparks -p '{"spec":{"to":{"name":"mlbparks-green"}}}' -n $GUID-parks-prod
-    else
-    echo "mlbparks-green svc currently"
-fi
-oc get route/nationalparks -n ${GUID}-parks-prod |grep 'nationalparks-blue'
-if [ $? == "nationalparks-blue" ]; then
-    echo "nationalparks-blue to nationalparks-green"
-    oc patch route/nationalparks -p '{"spec":{"to":{"name":"nationalparks-green"}}}' -n $GUID-parks-prod
-    else
-    echo "nationalparks-green svc currently"
-fi
-oc get route/parksmap -n ${GUID}-parks-prod |grep 'parksmap-blue'
-if [ $? == "parksmap-blue" ]; then
-    echo "parksmap-blue to parksmap-green"
-    oc patch route/parksmap -p '{"spec":{"to":{"name":"parksmap-green"}}}' -n $GUID-parks-prod
-    else
-    echo "parksmap-green svc currently"
-fi
-
+oc patch route/mlbparks -p '{"spec":{"to":{"name":"mlbparks-green"}}}' -n $GUID-parks-prod
+oc patch route/nationalparks -p '{"spec":{"to":{"name":"nationalparks-green"}}}' -n $GUID-parks-prod
+oc patch route/parksmap -p '{"spec":{"to":{"name":"parksmap-green"}}}' -n $GUID-parks-prod
